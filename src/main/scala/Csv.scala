0289ba571c36f90dc2f4d5d83406a64c257971bc
kei10in <kei10in@gmail.com>
2014-02-15 13:19:23 +0900

    parse field continuation

--- src/main/scala/Csv.scala
+++ src/main/scala/Csv.scala
@@ -1,7 +1,20 @@
 package scalacsv
 
-object Csv {
-  def parse(s: String) = {
-    List(List("1"))
+import scala.util.parsing.combinator._
+import scala.util.parsing.input.CharArrayReader.EofCh
+
+object Csv extends RegexParsers {
+  def apply(input: String): List[List[String]] = parseAll(table, input) match {
+    case Success(result, _) => result
+    case failure : NoSuccess => scala.sys.error(failure.msg)
   }
+
+  def table: Parser[List[List[String]]] = field ~ rep(continuous_field) ^^ {
+    case x ~ y => List(x :: y)
+  }
+
+  def continuous_field: Parser[String] = "," ~> field
+
+  def field: Parser[String] = """[^,]+""".r
+
 }
--- src/test/scala/CsvSpec.scala
+++ src/test/scala/CsvSpec.scala
@@ -2,13 +2,16 @@ package scalacsv
 
 import org.scalatest.FunSuite
 
-class HelloWorldSpec extends FunSuite {
-
-  def parse(s: String) = Csv.parse(s)
+class CsvSpec extends FunSuite {
 
   test("parse scalar") {
-    val actual = parse("1")
+    val actual = Csv("1")
     assert(actual === List(List("1")))
+  }
+
+  test("parse simple 10 fields") {
+    val actual = Csv("0,1,2,3,4,5,6,7,8,9")
+    assert(actual === List(List("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")))
   }
 
 }
